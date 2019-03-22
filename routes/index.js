8bd64c6c754c5483d7875aacc208610da73697a7
nipunbatra1994@gmail.com <nipunbatra1994@gmail.com>
2019-03-22 20:43:25 +0800

    fix index.js route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const books = require("../routes/books");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
   res.sendStatus(200);
 });
+
+router.use("/books", books);
 
 module.exports = router;
